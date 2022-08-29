617d314f478abec5f7628e61c51f05e6caa4d12a
EKamenyuk <e.kamenyuk@mail.ru>
2022-08-29 14:56:42 +0700

    "initially3"

---
+++ src/test/kotlin/MainKtTest.kt
@@ -0,0 +1,18 @@
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class MainKtTest {
+
+    @Test
+    fun processingCalc() {
+    }
+
+    @Test
+    fun convertRub() {
+    }
+
+    @Test
+    fun calcCommission() {
+    }
+}
