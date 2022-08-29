6ff802c304bfca7de51a42ab334dedc44f09ef43
EKamenyuk <e.kamenyuk@mail.ru>
2022-08-29 16:37:29 +0700

    "initially4"

--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -4,15 +4,46 @@ import org.junit.Assert.*
 
 class MainKtTest {
 
-    @Test
-    fun processingCalc() {
-    }
-
-    @Test
-    fun convertRub() {
-    }
 
     @Test
     fun calcCommission() {
+        val type = TYPE_1;
+        val amountOfPreviousPurchases = 123_000_00
+        val amountTransfer = 250_00
+        //val expectedResult = 23_00
+
+        val result = calcCommission(type, amountOfPreviousPurchases, amountTransfer)
+
+        assertEquals(3500, result)
+    }
+    @Test
+    fun calcCommission_2() {
+        val type = TYPE_1;
+        val amountOfPreviousPurchases = 150_000_00
+        val amountTransfer = 1500_00
+
+        val result = calcCommission(type, amountOfPreviousPurchases, amountTransfer)
+
+        assertEquals(11000, result)
+    }
+    @Test
+    fun calcCommission_3() {
+        val type = TYPE_2;
+        val amountOfPreviousPurchases = 80_000_00
+        val amountTransfer = 100_00
+
+        val result = calcCommission(type, amountOfPreviousPurchases, amountTransfer)
+
+        assertEquals(3500, result)
+    }
+    @Test
+    fun processingCalc() {
+        val type = TYPE_3;
+        val amountPurchases = 80_000_00
+        val amountTransfer = 100_00
+
+        val result = calcCommission(type, amountPurchases, amountTransfer)
+
+        assertEquals(3500, result)
     }
 }
